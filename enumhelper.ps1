2c97edd32019a5c9a21ab2b76ecf4a114c023b29
LancreFI <50398030+LancreFI@users.noreply.github.com>
2023-12-03 00:18:59 +0200

    enumhelper.ps1

--- enumhelper.ps1
+++ enumhelper.ps1
@@ -3,6 +3,7 @@
 
     A small helper script to help in AD enumeration.
 #>
+#Parameters
 param (
     [Parameter(Mandatory=$true)]
     [string]$ldap_query,
@@ -10,6 +11,7 @@ param (
 	[Parameter(Mandatory=$false)]
 	[string]$ldap_property = "l")
 
+#Script help
 if ($ldap_query.ToUpper() -eq "HELP" -or $ldap_query.ToUpper() -eq "H")
 {
   Write-Host "
@@ -37,11 +39,24 @@ if ($ldap_query.ToUpper() -eq "HELP" -or $ldap_query.ToUpper() -eq "H")
   
   Also some other stuff can be done with '<param1>':
   '<param1>' == 'dcinfo'     -->  Prints out DC name, hostname, OS, OS version, IPv4 and IPv6
-  '<param1>' == 'deviceinfo' -->  Prints out AD devices' info: Name, hostname, OS, OS SP, OS version, IPv4 and IPv6
+  '<param1>' == 'deviceinfo' -->  Prints out AD devices' info: Name, hostname, OS, OS SP, OS version, 
+                                  IPv4 and IPv6
   
   For example:
   .\enumhelper.ps1 'dcinfo'
   .\enumhelper.ps1 'deviceinfo'
+  
+  '<param1>' == 'spncheck' '<param2>' == 'account' -->  Check if any SPNs (services) are tied to a 
+                                                        (service)account
+  '<param1>' == 'checksid' '<param2>' == 'SID'     -->  Return the AD user / group info by SID, can be 
+                                                        used to convert a SID to name for example
+  '<param1>' == 'findsid'  '<param2>' == 'SID'     -->  Look through the whole AD for a SID match if you
+                                                        don't know the object type (=user/group/etc)
+  
+  For example:
+  .\enumhelper.ps1 'spncheck' 'sql_service'
+  .\enumhelper.ps1 'checksid' 'S-1-5-21-809893099-1472282828-2400958209-1105'
+  .\enumhelper.ps1 'findsid' 'S-1-5-21-809893099-1472282828-2400958209-1105'
   
 <------------------------------------------------------------------------------------------------------->
   
@@ -60,21 +75,95 @@ if ($ldap_query.ToUpper() -eq "HELP" -or $ldap_query.ToUpper() -eq "H")
   #SAM_ACCOUNT_TYPE_MAX 0x7fffffff --------------> 2147483647
 
 <------------------------------------------------------------------------------------------------------->
+
+  Interesting Access Control Entry (ACE) types and descriptions:
+  # AllExtendedRights = Change and reset password etc.
+  # ForceChangePassword = Can change the password for an object
+  # GenericAll = Full permissions on an object
+  # GenericWrite = Edit some of the object's attributes
+  # Self (Self-Membership) = Add ourselves to a group for example
+  # WriteOwner = Change object ownership
+  # WriteDACL = Edit ACEs applied to object
+  
+ <------------------------------------------------------------------------------------------------------->
+ 
   "
   exit
 }
+#Check the info for the DC
 elseif ($ldap_query.ToUpper() -eq "DCINFO")
 {
 	Get-ADDomainController -Filter * | Format-Table Name, Hostname, OperatingSystem, OperatingSystemVersion, IPv4Address, IPv6Address -Wrap -Auto
 	exit
 }
+#Check the info for computers tied to the AD
 elseif ($ldap_query.ToUpper() -eq "DEVICEINFO")
 {
 	Get-ADComputer -Filter * -Property * `
 	| Format-Table Name, DNSHostname, OperatingSystem, OperatingSystemServicePack, OperatingSystemVersion, IPv4Address, IPv6Address -Wrap -Auto	
 	exit
 }
+#Check SPN ties for account defined as the second parameter on commandline
+elseif ($ldap_query.ToUpper() -eq "CHECKSPN")
+{
+	if ($ldap_property.ToUpper() -ne "L")
+	{
+		setspn -L $ldap_property
+	}
+	else
+	{
+		Write-Host 'Check the service account name!'
+	}
+	exit
+}
+#Convert SID to user/group
+elseif ($ldap_query.ToUpper() -eq "CHECKSID" -or $ldap_query.ToUpper() -eq "FINDSID")
+{
+	if ($ldap_property.ToUpper() -ne "L")
+	{
+		if ($ldap_query.ToUpper() -eq "FINDSID")
+		{
+			$result = Get-ADObject -IncludeDeletedObjects -Filter "objectSid -eq '$ldap_property'"|select Name,ObjectClass
+			if (-not $result)
+			{
+				Write-Host 'Nothing matching that SID in AD'
+			}
+			else
+			{	
+				$result	
+			}
+		}
+		else
+		{
+			Write-Host ''
+			Write-Host '----CHECKING FROM USERS----'
+			try{
+				Get-ADUser -Identity $ldap_property
+			}
+			catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
+			{
+				Write-Host 'The SID was not found from the AD users!'
+			}
+			Write-Host ''
+			Write-Host '----CHECKING FROM GROUPS----'
+			try{
+				Get-ADGroup -Identity $ldap_property
+			}
+			catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
+			{
+				Write-Host 'The SID was not found from AD members!'
+			}
+			Write-Host ''
+		}
+	}
+	else
+	{
+		Write-Host 'Recheck the SID!'
+	}
+	exit
+}
 
+#LDAP-search  function
 function ldap_search {	
     $pdc = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
     $dn = ([adsi]'').distinguishedName
@@ -86,12 +175,15 @@ function ldap_search {
 
 $ldap_query_res = ldap_search
 
+#Print the LDAP-query results based on the paramaters from commandline
 Write-Host ""
 Write-Host "###--------------RESULTS--------------------###"
+#Full query
 if ($ldap_property -eq "")
 {
 	return $ldap_query_res
 }
+#List properties for the queried object
 elseif ($ldap_property.ToUpper() -eq "L")
 {
 	foreach ($query_object in $ldap_query_res)
@@ -102,6 +194,7 @@ elseif ($ldap_property.ToUpper() -eq "L")
 		Write-Host "###-----------------------------------------###"
 	}
 }
+#List the queried objects values for a specific item
 else
 {
 	foreach ($query_object in $ldap_query_res) 
