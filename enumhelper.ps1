650f330a9ca8b584669b3dd25f153df5f42813c4
LancreFI <50398030+LancreFI@users.noreply.github.com>
2023-12-12 00:28:13 +0200

    enumhelper.ps1

--- enumhelper.ps1
+++ enumhelper.ps1
@@ -5,24 +5,30 @@
 #>
 #Parameters
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$ldap_query,
+	[Parameter(Mandatory=$true)]
+    	[string]$param1,
 	
 	[Parameter(Mandatory=$false)]
-	[string]$ldap_property = "l",
+	[string]$param2 = "l",
 	
 	[Parameter(Mandatory=$false)]
-	[string]$ldap_pasw)
+	[string]$param3,
+	
+	[Parameter(Mandatory=$false)]
+	[string]$param4,
+	
+	[Parameter(Mandatory=$false)]
+	[string]$param5)
 
 #Script help
-if ($ldap_query.ToUpper() -eq "HELP" -or $ldap_query.ToUpper() -eq "H")
+if ($param1.ToUpper() -eq "HELP" -or $param1.ToUpper() -eq "H")
 {
   Write-Host "
 <------------------------------------------------------------------------------------------------------->
   
- Usage: .\enumhelper.ps1 '<param1>' '<param2>' '<param3>'
+ Usage: .\enumhelper.ps1 '<param1>' '<param2>' '<param3>' '<param4>' '<param5>'
   
-  The '<param1>' is mandatory, if not defined will be prompted, '<param2>' & '<param3>' are optional.
+  The '<param1>' is mandatory, if not defined will be prompted, the rest are optional.
  
 <------------------------------------------------------------------------------------------------------->
   
@@ -36,41 +42,8 @@ if ($ldap_query.ToUpper() -eq "HELP" -or $ldap_query.ToUpper() -eq "H")
   For example:
   .\enumhelper.ps1 '(&(SamAccountType=805306368)(name=p00p))' 'L'
   or
-  .\enumhelper.ps1 '(&(SamAccountType=805306368)(name=p00p))' 'whencreated'  
+  .\enumhelper.ps1 '(&(SamAccountType=805306368)(name=p00p))' 'whencreated'
   
-<------------------------------------------------------------------------------------------------------->
-  
-  Also some other stuff can be done with '<param1>':
-  '<param1>' == 'spnlist'    -->  Prints out all SPNs and related objects
-  '<param1>' == 'dcinfo'     -->  Prints out DC name, hostname, OS, OS version, IPv4 and IPv6
-  '<param1>' == 'deviceinfo' -->  Prints out AD devices' info: Name, hostname, OS, OS SP, OS version, 
-                                  IPv4 and IPv6
-  
-  For example:
-  .\enumhelper.ps1 'spnlist'
-  .\enumhelper.ps1 'dcinfo'
-  .\enumhelper.ps1 'deviceinfo'
-  
-  '<param1>' == 'spncheck' '<param2>' == 'account' -->  Check if any SPNs (services) are tied to a 
-                                                        (service)account
-  '<param1>' == 'checksid' '<param2>' == 'SID'     -->  Return the AD user / group info by SID, can be 
-                                                        used to convert a SID to name for example
-  '<param1>' == 'findsid'  '<param2>' == 'SID'     -->  Look through the whole AD for a SID match if you
-                                                        don't know the object type (=user/group/etc)
-  
-  For example:
-  .\enumhelper.ps1 'spncheck' 'sql_service'
-  .\enumhelper.ps1 'checksid' 'S-1-5-21-809893099-1472282828-2400958209-1105'
-  .\enumhelper.ps1 'findsid' 'S-1-5-21-809893099-1472282828-2400958209-1105'
-  
-<------------------------------------------------------------------------------------------------------->
-  
-  And then there's '<param3>' which can be used to do user/pass queries against the AD:
-  '<param1>' == 'auth' '<param2>' == 'username'  '<param3>' == 'password'
-  
-  For example:
-  .\enumhelper.ps1 'auth' 'megaAdmin' 'verySecurePassword'
-
 <------------------------------------------------------------------------------------------------------->
   
   For reminder, the SamAccountTypes to query for, you need to use the decimal notation:
@@ -85,43 +58,143 @@ if ($ldap_query.ToUpper() -eq "HELP" -or $ldap_query.ToUpper() -eq "H")
   #SAM_TRUST_ACCOUNT 0x30000002 -----------------> 805306370
   #SAM_APP_BASIC_GROUP 0x40000000 ---------------> 1073741824
   #SAM_APP_QUERY_GROUP 0x40000001 ---------------> 1073741825
-  #SAM_ACCOUNT_TYPE_MAX 0x7fffffff --------------> 2147483647
-
-<------------------------------------------------------------------------------------------------------->
-
-  Interesting Access Control Entry (ACE) types and descriptions:
-  # AllExtendedRights = Change and reset password etc.
-  # ForceChangePassword = Can change the password for an object
-  # GenericAll = Full permissions on an object
-  # GenericWrite = Edit some of the object's attributes
-  # Self (Self-Membership) = Add ourselves to a group for example
-  # WriteOwner = Change object ownership
-  # WriteDACL = Edit ACEs applied to object
+  #SAM_ACCOUNT_TYPE_MAX 0x7fffffff --------------> 2147483647  
   
- <------------------------------------------------------------------------------------------------------->
+<------------------------------------------------------------------------------------------------------->
+  
+  Also some other stuff can be done with '<param1>':
+  '<param1>' == 'spnlist'    -->  Prints out all SPNs and related objects
+  '<param1>' == 'dcinfo'     -->  Prints out DC name, hostname, OS, OS version, IPv4 and IPv6
+  '<param1>' == 'deviceinfo' -->  Prints out AD devices' info: Name, hostname, OS, OS SP, OS version, 
+                                  IPv4 and IPv6
+  
+  For example:
+  .\enumhelper.ps1 'spnlist'
+  .\enumhelper.ps1 'dcinfo'
+  .\enumhelper.ps1 'deviceinfo'
+ 
+<------------------------------------------------------------------------------------------------------->
+ 
+  Leveraging further with '<param2>':
+  '<param1>' == 'spncheck' '<param2>' == 'account' -->  Check if any SPNs (services) are tied to a 
+                                                        (service)account
+  '<param1>' == 'checksid' '<param2>' == 'SID'     -->  Return the AD user / group info by SID, can be 
+                                                        used to convert a SID to name for example
+  '<param1>' == 'findsid'  '<param2>' == 'SID'     -->  Look through the whole AD for a SID match if you
+                                                        don't know the object type (=user/group/etc)
+  
+  For example:
+  .\enumhelper.ps1 'spncheck' 'sql_service'
+  .\enumhelper.ps1 'checksid' 'S-1-5-21-809893099-1472282828-2400958209-1105'
+  .\enumhelper.ps1 'findsid' 'S-1-5-21-809893099-1472282828-2400958209-1105'
+ 
+<------------------------------------------------------------------------------------------------------->
+ 
+  And then there's '<param3>' which can be used to do user/pass queries against the AD:
+  '<param1>' == 'auth' '<param2>' == 'username'  '<param3>' == 'password'
+  
+  For example:
+  .\enumhelper.ps1 'auth' 'megaAdmin' 'verySecurePassword'
+ 
+<------------------------------------------------------------------------------------------------------->
+ 
+  With some additional parameters '<param4>' and '<param5>' we can try to run remote commands over WMI:
+  '<param1>' == 'wmi' '<param2>' == 'username'  '<param3>' == 'password'  '<param4>' == target IP
+  '<param5>' == remote command to run
+  
+  For example:
+  .\enumhelper.ps1 'wmi' 'username' 'password' '192.168.12.34' 'cmd'
+  !NOTE: you can just run with 'wmi' and the rest will be prompted, if you don't want to plaintext the
+         password on commandline.
+  
+<------------------------------------------------------------------------------------------------------->
  
   " -BackgroundColor Green -ForegroundColor Black
   exit
 }
+elseif ($param1.ToUpper() -eq "WMI")
+{
+	if ($param2 -ne "1" -and $param2 -and $param3)
+	{
+		$username = $param2
+		$insecure_password = $param3
+		$password = ConvertTo-SecureString $insecure_password -AsPlaintext -Force
+		$computer = $param4
+		$command = $param5
+	}
+	else
+	{
+		while (-not $username -or -not $password -or -not $computer -or -not $command)
+		{
+			$username = Read-Host -prompt 'Username: '
+			$password = Read-Host -prompt 'Password: ' -AsSecureString
+			$computer = Read-Host -prompt 'Target IP: '
+			$command = Read-Host -prompt 'Command to run: '
+		}
+	}
+	Write-Host ""
+	Write-Host "###--------------WMIC-RUN--------------------###" -ForegroundColor Yellow
+	Write-Host ""
+	try
+	{
+		$credential = New-Object System.Management.Automation.PSCredential $username, $password
+		$options = New-CimSessionOption -Protocol DCOM -ErrorAction stop
+		$params = @{'ComputerName'=$computer
+			'Credential'=$credential
+            		'SessionOption'=$options
+            		'ErrorAction'='Stop'}
+		$session = New-CimSession @params
+	}
+	catch [Microsoft.Management.Infrastructure.CimException]
+	{
+		if ($_.Exception.Message -like '*Access*Denied*')
+		{
+			Write-Host "Invalid credentials!" -ForegroundColor Red
+		}
+		elseif ($_.Exception.Message -like '*RPC*server*unavailable*')
+		{
+			Write-Host "The host is unreachable: $computer" -ForegroundColor Red
+		}
+		elseif ($_.Exception.Message -like '*your*domain*isn*t*available*')
+		{
+			Write-Host "Domain unreachable, is the DC down?!" -ForegroundColor Red
+		}
+		else
+		{
+			$_.Exception.Message
+		}
+	}
+	try
+	{
+		Invoke-CimMethod -CimSession $session -ClassName Win32_Process -MethodName Create `
+		-Arguments @{CommandLine=$Command}
+	}
+	catch [System.Management.Automation.ParameterBindingException]
+	{
+		Write-Host "Cim-Session failed!" -ForeGroundColor Red
+	}
+	Write-Host ""
+	exit
+}
 #Check the info for the DC
-elseif ($ldap_query.ToUpper() -eq "DCINFO")
+elseif ($param1.ToUpper() -eq "DCINFO")
 {
 	Get-ADDomainController -Filter * | Format-Table Name, Hostname, OperatingSystem, OperatingSystemVersion, IPv4Address, IPv6Address -Wrap -Auto
 	exit
 }
 #Check the info for computers tied to the AD
-elseif ($ldap_query.ToUpper() -eq "DEVICEINFO")
+elseif ($param1.ToUpper() -eq "DEVICEINFO")
 {
 	Get-ADComputer -Filter * -Property * `
 	| Format-Table Name, DNSHostname, OperatingSystem, OperatingSystemServicePack, OperatingSystemVersion, IPv4Address, IPv6Address -Wrap -Auto	
 	exit
 }
 #Check SPN ties for account defined as the second parameter on commandline
-elseif ($ldap_query.ToUpper() -eq "CHECKSPN")
+elseif ($param1.ToUpper() -eq "CHECKSPN")
 {
-	if ($ldap_property.ToUpper() -ne "L")
+	if ($param2.ToUpper() -ne "L")
 	{
-		setspn -L $ldap_property
+		setspn -L $param2
 	}
 	else
 	{
@@ -130,13 +203,13 @@ elseif ($ldap_query.ToUpper() -eq "CHECKSPN")
 	exit
 }
 #Convert SID to user/group
-elseif ($ldap_query.ToUpper() -eq "CHECKSID" -or $ldap_query.ToUpper() -eq "FINDSID")
+elseif ($param1.ToUpper() -eq "CHECKSID" -or $param1.ToUpper() -eq "FINDSID")
 {
-	if ($ldap_property.ToUpper() -ne "L")
+	if ($param2.ToUpper() -ne "L")
 	{
-		if ($ldap_query.ToUpper() -eq "FINDSID")
+		if ($param1.ToUpper() -eq "FINDSID")
 		{
-			$result = Get-ADObject -IncludeDeletedObjects -Filter "objectSid -eq '$ldap_property'"|select Name,ObjectClass
+			$result = Get-ADObject -IncludeDeletedObjects -Filter "objectSid -eq '$param2'"|select Name,ObjectClass
 			if (-not $result)
 			{
 				Write-Host 'Nothing matching that SID in AD' -ForegroundColor Red
@@ -151,7 +224,7 @@ elseif ($ldap_query.ToUpper() -eq "CHECKSID" -or $ldap_query.ToUpper() -eq "FIND
 			Write-Host ''
 			Write-Host '----CHECKING FROM USERS----'  -ForegroundColor Yellow
 			try{
-				Get-ADUser -Identity $ldap_property
+				Get-ADUser -Identity $param2
 			}
 			catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
 			{
@@ -160,7 +233,7 @@ elseif ($ldap_query.ToUpper() -eq "CHECKSID" -or $ldap_query.ToUpper() -eq "FIND
 			Write-Host '' 
 			Write-Host '----CHECKING FROM GROUPS----'  -ForegroundColor Yellow
 			try{
-				Get-ADGroup -Identity $ldap_property
+				Get-ADGroup -Identity $param2
 			}
 			catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException]
 			{
@@ -181,9 +254,9 @@ function ldap_search {
     $pdc = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().PdcRoleOwner.Name
     $dn = ([ADSI]'').distinguishedName
     $ldap_path = "LDAP://$pdc/$dn"
-    if ($ldap_query.ToUpper() -eq "AUTH")
+    if ($param1.ToUpper() -eq "AUTH")
 	{
-		$ldap_login_res = New-Object System.DirectoryServices.DirectoryEntry($ldap_path, $ldap_property, $ldap_pasw)
+		$ldap_login_res = New-Object System.DirectoryServices.DirectoryEntry($ldap_path, $param2, $param3)
 		if (-not $ldap_login_res.DistinguishedName)
 		{
 			return $false
@@ -193,7 +266,7 @@ function ldap_search {
 			return $true
 		}
 	}
-	elseif ($ldap_query.ToUpper() -eq "SPNLIST")
+	elseif ($param1.ToUpper() -eq "SPNLIST")
 	{
 		$spn_search = New-Object DirectoryServices.DirectorySearcher([ADSI]"")
 		$spn_search.filter = "(servicePrincipalName=*)"
@@ -203,38 +276,38 @@ function ldap_search {
 	else
 	{
 		$dir_entry = New-Object System.DirectoryServices.DirectoryEntry($ldap_path)
-		$dir_searcher = New-Object System.DirectoryServices.DirectorySearcher($dir_entry, $ldap_query)	
+		$dir_searcher = New-Object System.DirectoryServices.DirectorySearcher($dir_entry, $param1)	
 		return $dir_searcher.FindAll()
 	}
 }
 
-$ldap_query_res = ldap_search
+$param1_res = ldap_search
 
 #Print the LDAP-query results based on the paramaters from commandline
 Write-Host ""
 Write-Host "###--------------RESULTS--------------------###" -ForegroundColor Yellow
 Write-Host ""
 #Full query
-if ($ldap_property -eq "" -or $ldap_query.ToUpper() -eq "AUTH")
+if ($param2 -eq "" -or $param1.ToUpper() -eq "AUTH")
 {
-	if ($ldap_query_res)
+	if ($param1_res)
 	{
-		Write-Host 'Credentials match ('$ldap_property' / '$ldap_pasw' )' -ForegroundColor Green
+		Write-Host 'Credentials match ('$param2' / '$param3' )' -ForegroundColor Green
 		Write-Host ""
 	}
-	elseif (-not $ldap_query_res)
+	elseif (-not $param1_res)
 	{
-		Write-Host 'Credentials do not match ('$ldap_property' / '$ldap_pasw' )' -ForegroundColor Red
+		Write-Host 'Credentials do not match ('$param2' / '$param3' )' -ForegroundColor Red
 		Write-Host ""
 	}
 	else
 	{
-		return $ldap_query_res
+		return $param1_res
 	}
 }
-elseif ($ldap_query.ToUpper() -eq "SPNLIST")
+elseif ($param1.ToUpper() -eq "SPNLIST")
 {
-	foreach( $result in $ldap_query_res )
+	foreach( $result in $param1_res )
 	{
 		$userEntry = $result.GetDirectoryEntry()
 		Write-host "Object Name =  "	$userEntry.name -ForeGroundColor "black" -BackgroundColor "gray"
@@ -244,16 +317,16 @@ elseif ($ldap_query.ToUpper() -eq "SPNLIST")
 		$i=1
 		foreach( $SPN in $userEntry.servicePrincipalName ) 
 		{
-			Write-host "SPN ${i} = $SPN" -ForeGroundColor "yellow" -BackgroundColor "gray"
+			Write-host "SPN ${i} = $SPN" -ForeGroundColor "red" -BackgroundColor "gray"
 			$i+=1
 		}
 		Write-host ""
 	}
 }
 #List properties for the queried object
-elseif ($ldap_property.ToUpper() -eq "L")
+elseif ($param2.ToUpper() -eq "L")
 {
-	foreach ($query_object in $ldap_query_res)
+	foreach ($query_object in $param1_res)
 	{
 		Write-Host ""
 		$query_object.properties
@@ -264,10 +337,10 @@ elseif ($ldap_property.ToUpper() -eq "L")
 #List the queried objects values for a specific item
 else
 {
-	foreach ($query_object in $ldap_query_res) 
+	foreach ($query_object in $param1_res) 
 	{
 		Write-Host ""
-		$query_object.properties.item($ldap_property)
+		$query_object.properties.item($param2)
 		Write-Host ""
 		Write-Host "###-----------------------------------------###" -ForegroundColor Yellow
 	}
